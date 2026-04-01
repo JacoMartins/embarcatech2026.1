d9bcf6ea3f259255e5bd1bc006934125e303218e
Jacó Alves <developer@localhost.localdomain>
2026-04-01 01:59:39 +0000

    feat: item11

---
+++ item-1/item-1.c
@@ -0,0 +1,53 @@
+#include "stdio.h"
+#include "pico/stdlib.h"
+#include "hardware/timer.h"
+#include <hardware/gpio.h>
+
+#define button_a 5
+#define led_b 12
+
+bool repeating_timer_callback(struct repeating_timer *t) {
+    return true;
+}
+
+int main()
+{
+    gpio_init(led_b);
+    gpio_set_dir(led_b, GPIO_OUT);
+    gpio_put(led_b, false);
+
+    gpio_init(button_a);
+    gpio_set_dir(button_a, GPIO_IN);
+    gpio_pull_up(button_a);
+
+    int count = 0;
+    bool blinking = false;
+    bool led_state = false;
+    bool last_button_state = true;
+
+    struct repeating_timer timer;
+
+    while (true) {
+        bool button_a_state = gpio_get(button_a);
+
+        if (last_button_state == true && button_a_state == false) {           
+            count++;
+        }
+        
+        last_button_state = button_a_state;
+        
+        if (count >= 5) {
+            blinking = true;
+
+            while (blinking) {
+                gpio_put(led_b, 255);
+                add_repeating_timer_ms(500, repeating_timer_callback, NULL, &timer);
+                gpio_put(led_b, 0);
+                add_repeating_timer_ms(500, repeating_timer_callback, NULL, &timer);
+            }
+            gpio_put(led_b, 0);
+        }
+
+        sleep_ms(10); // delay para evitar leituras inconsistentes,
+    }
+}
